c385adf06d6cf7c74b8611ac6c872ea756f4f69d
Ebuka Ezike <yerimyah1@gmail.com>
2024-01-30 19:45:36 +0000

    Add: Rectangle colour changed by keypress

--- snake.asm
+++ snake.asm
@@ -1,3 +1,27 @@
+	section .bss
+struc SDL_Rect
+	.x: resd 1
+	.y: resd 1
+	.w: resd 1
+	.h: resd 1
+	.size:
+endstruc
+
+struc SDL_Color
+	.r: resb 1
+	.g: resb 1
+	.b: resb 1
+	.a: resb 1
+	.size:
+endstruc
+
+struc SDL_Event
+	.type: resd 1
+	.space: resd 4
+	.sym: resd 1
+	.size:
+endstruc
+
 	section .text
 extern SDL_Init
 extern SDL_CreateWindow
@@ -5,6 +29,8 @@ extern SDL_CreateRenderer
 extern SDL_SetRenderDrawColor
 extern SDL_RenderClear
 extern SDL_RenderPresent
+extern SDL_RenderDrawRect
+extern SDL_RenderFillRect
 extern SDL_GetError
 extern SDL_PollEvent
 extern SDL_DestroyWindow
@@ -16,6 +42,8 @@ main:
 	push	rbp
 	mov	rbp, rsp
 	xor	rax, rax
+	sub	rsp, 16; space for color
+	sub	rsp, SDL_Rect.size ; space for the rect
 	sub	rsp, 64 ; create stack space for SDL_Event(56)
 
 	; init SDL 
@@ -44,18 +72,18 @@ main:
 	cmp	r13, 0x0
 	je	handle_error 
 
-    ; set renderer color 
-	mov	rdi, r13 
-	mov	rsi, 0
-	xor 	rdx, rdx
-	mov 	rcx, 0xff
-	mov 	r8, 0xff
-	call 	SDL_SetRenderDrawColor
-
 	mov 	rdi, r13
 	call 	SDL_RenderClear
-	mov 	rdi, r13
-	call 	SDL_RenderPresent
+
+	; set renderer color 
+	mov	rdi, r13 
+	mov	esi, 0
+	xor 	edx, edx
+	mov 	ecx, 0xff
+	mov 	r8d, 0xff
+	call 	SDL_SetRenderDrawColor
+
+
 
 	; delay for 5 seconds
 	; mov rdi, 5000
@@ -73,14 +101,28 @@ main:
 game_loop:
 	lea	rdi, [rsp + 8]
 	call	SDL_PollEvent ; poll events
-		
 	cmp	eax, 0
 	jz	game_loop
 
-	mov	edi, [rsp + 8]
-	cmp	edi, [SDL_QUIT]
+	mov	r10d, [rsp + 8] 
+	cmp	r10d, [SDL_QUIT] ; if quit is pressed
 	je	game_loop_end
 
+	cmp	r10d, [SDL_KEYDOWN] 
+	je	handle_keypress	
+
+render_present:
+	
+	mov	dword [rsp + 64 + SDL_Rect.x], 10
+	mov	dword [rsp + 64 + SDL_Rect.y], 10
+	mov	dword [rsp + 64 + SDL_Rect.w], 100
+	mov	dword [rsp + 64 + SDL_Rect.h], 100
+	mov	rdi, r13
+	lea	rsi, [rsp + 64]
+	call 	SDL_RenderFillRect
+
+	mov 	rdi, r13
+	call 	SDL_RenderPresent
 	jmp	game_loop
 game_loop_end:
 
@@ -88,19 +130,51 @@ sdl_cleanup:
 	mov	rdi, r12
 	call	SDL_DestroyWindow
 	call	SDL_Quit
-	jmp	main_function_end
+
+main_function_end:
+	xor	rax, rax
+	add	rsp, 64
+	add	rsp, SDL_Rect.size
+	add	rsp, 16
+	leave
+	ret
 
 handle_error:
 	call	SDL_GetError
 	mov	rsi, rax
 	mov	edi, str_error
 	call	printf
+	jmp	main_function_end
 
-main_function_end:
-	xor	rax, rax
-	add	rsp, 64
-	leave
-	ret
+handle_keypress:
+	mov 	r10d,[rsp + 8 + SDL_Event.sym]
+	cmp	r10d, [SDLK_UP]
+	mov	eax, [RED]
+	je	.set_colour
+	cmp	r10d, [SDLK_DOWN]
+	mov	eax, [GREEN]
+	je	.set_colour
+	cmp	r10d, [SDLK_LEFT]
+	mov	eax, [BLUE]
+	je	.set_colour
+	cmp	r10d, [SDLK_RIGHT]
+	mov	eax, [YELLOW]
+	je	.set_colour
+	mov	eax, 0x111111ff
+	.set_colour:
+
+
+show_color:	
+	mov	rdi, r13 ; renderer
+	movzx	r8d, al ; a
+	shr	rax, 8
+	movzx	ecx, al ; b
+	shr	rax, 8
+	movzx	edx, al; r
+	shr	rax, 8
+	movzx	rsi, al; g
+	call	SDL_SetRenderDrawColor
+	jmp 	render_present
 
 
 ; ---- [ SECTION RODATA ] ----
@@ -109,6 +183,16 @@ main_function_end:
 SDL_INIT_VIDEO:	dd 0x000020
 SDL_WINDOWPOS_CENTERED: dd 0x2FFF0000
 SDL_QUIT: dd 0x100
+SDLK_RIGHT: dd 0x4000004f
+SDLK_LEFT: dd 0x40000050
+SDLK_DOWN: dd 0x40000051
+SDLK_UP: dd 0x40000052
+SDL_KEYDOWN: dd 0x300
+
+RED: dq 0xff0000ff
+GREEN: dq 0x00ff00ff
+BLUE: dq 0x0000ffff
+YELLOW: dq 0xffff00ff
 window_width: dd 800
 window_height: dd 600
 window_title: db "This is the assembly code", 0
